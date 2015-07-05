e8f480b0b37fc277b4b86bb2b9f95409234d3912
Andrew.Albers <andrewalbers@WF12249.local>
2015-07-04 22:11:51 -0600

    changed hello world to alien empire

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(require('./lib/appengine-handlers'));
 // [START hello_world]
 /* Say hello! */
 app.get('/', function(req, res) {
-  res.status(200).send("Hello, world!");
+i  res.status(200).send("Alien Empire!");
 });
 // [END hello_world]
 
