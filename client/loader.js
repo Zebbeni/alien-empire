155bde75134a6ca0c3360d184b15f55ffbcb0be9
Zebbeni <andrew.albers@gmail.com>
2015-10-01 22:23:42 -0600

    redid variable url

--- client/loader.js
+++ client/loader.js
@@ -8,49 +8,49 @@ var load_assets = function() {
 
 	if (!is_all_loaded){
 
-		// var url = "https://s3-us-west-2.amazonaws.com/alien-empire/";
+		var url = "https://s3-us-west-2.amazonaws.com/alien-empire/";
 
 		manifest = [
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/metal.png", id: "metal"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/water.png", id: "water"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/fuel.png", id: "fuel"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/food.png", id: "food"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/stars.png", id: "stars"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/asteroids.png", id: "asteroids"},
+			{src: url + "game/metal.png", id: "metal"},
+			{src: url + "game/water.png", id: "water"},
+			{src: url + "game/fuel.png", id: "fuel"},
+			{src: url + "game/food.png", id: "food"},
+			{src: url + "game/stars.png", id: "stars"},
+			{src: url + "game/asteroids.png", id: "asteroids"},
 
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/arrow_red.png", id: "arrow_color0"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/arrow_blue.png", id: "arrow_color1"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/arrow_green.png", id: "arrow_color2"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/arrow_yellow.png", id: "arrow_color3"},
+			{src: url + "game/arrow_red.png", id: "arrow_color0"},
+			{src: url + "game/arrow_blue.png", id: "arrow_color1"},
+			{src: url + "game/arrow_green.png", id: "arrow_color2"},
+			{src: url + "game/arrow_yellow.png", id: "arrow_color3"},
 
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/mine_red.png", id: "mine0"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/mine_blue.png", id: "mine1"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/mine_green.png", id: "mine2"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/mine_yellow.png", id: "mine3"},
+			{src: url + "game/mine_red.png", id: "mine0"},
+			{src: url + "game/mine_blue.png", id: "mine1"},
+			{src: url + "game/mine_green.png", id: "mine2"},
+			{src: url + "game/mine_yellow.png", id: "mine3"},
 
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/factory_red.png", id: "factory0"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/factory_blue.png", id: "factory1"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/factory_green.png", id: "factory2"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/factory_yellow.png", id: "factory3"},
+			{src: url + "game/factory_red.png", id: "factory0"},
+			{src: url + "game/factory_blue.png", id: "factory1"},
+			{src: url + "game/factory_green.png", id: "factory2"},
+			{src: url + "game/factory_yellow.png", id: "factory3"},
 			
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/embassy_red.png", id: "embassy0"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/embassy_blue.png", id: "embassy1"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/embassy_green.png", id: "embassy2"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/embassy_yellow.png", id: "embassy3"},
+			{src: url + "game/embassy_red.png", id: "embassy0"},
+			{src: url + "game/embassy_blue.png", id: "embassy1"},
+			{src: url + "game/embassy_green.png", id: "embassy2"},
+			{src: url + "game/embassy_yellow.png", id: "embassy3"},
 
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/base_red.png", id: "base0"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/base_blue.png", id: "base1"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/base_green.png", id: "base2"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/base_yellow.png", id: "base3"},
+			{src: url + "game/base_red.png", id: "base0"},
+			{src: url + "game/base_blue.png", id: "base1"},
+			{src: url + "game/base_green.png", id: "base2"},
+			{src: url + "game/base_yellow.png", id: "base3"},
 
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/fleet_red.png", id: "fleet0"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/fleet_blue.png", id: "fleet1"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/fleet_green.png", id: "fleet2"},
-			{src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/fleet_yellow.png", id: "fleet3"}
+			{src: url + "game/fleet_red.png", id: "fleet0"},
+			{src: url + "game/fleet_blue.png", id: "fleet1"},
+			{src: url + "game/fleet_green.png", id: "fleet2"},
+			{src: url + "game/fleet_yellow.png", id: "fleet3"}
 		];
 
 		for ( var p = 1; p <= 29; p++ ) {
-			manifest.push({src: "https://s3-us-west-2.amazonaws.com/alien-empire/game/planet_" + p + ".png", id: "planet_" + p });
+			manifest.push({src: url + "game/planet_" + p + ".png", id: "planet_" + p });
 		}
 
 		loader = new createjs.LoadQueue(true, null, true);
