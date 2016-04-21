68a4e9e988f440f3ef8a245a2d4cde24be03a0ca
Zebbeni <andrew.albers@gmail.com>
2016-04-20 23:37:10 -0600

    fixed bug building on newly explored planets

--- game_actions.js
+++ game_actions.js
@@ -1142,10 +1142,8 @@ var applyBlockMission = function( action, game ){
 										  planetid, 
 										  cons.PNT_EXPLORE, 
 										  game );
-						game.board.planets[planetid].explored = true;
-						for (var p = 0; p < game.players.length; p++){
-							updatePlanetBuildable(player, game, planetid);
-						}
+
+						setPlanetExplored( game, planetid );
 					}
 
 					var is_unreserved = false;
@@ -1318,7 +1316,7 @@ var applyMissionResolve = function( action, game ){
 						 	 response: "This resource is already reserved"
 					};
 				}
-								
+				
 				resource.reserved = player;
 				break;
 
@@ -1714,6 +1712,14 @@ var removeAllFleets = function( game, player ){
 	}
 };
 
+var setPlanetExplored = function( game, planetid ){
+	game.board.planets[planetid].explored = true;
+
+	for ( var p = 0; p < game.players.length; p++ ){
+		updateBuildable( p, game );
+	}
+};
+
 // Updates planet.settledBy[player] to true or false 
 // 
 // True if player has a non-space structure on this planet
@@ -1764,7 +1770,7 @@ var updatePlanetBuildable = function( player, game, planetid ) {
 		for ( var pid in planets[planetid].borders ){
 			// if border open with this planet (not unexplored or blocked)
 			if ( planets[pid].settledBy[player] 
-				 && planets[planetid].borders[pid] == cons.BRD_OPEN ){
+				 && planets[planetid].borders[pid] != cons.BRD_BLOCKED ){
 				// set buildableBy to true for this player
 				planets[planetid].buildableBy[player] = true;
 				break;
